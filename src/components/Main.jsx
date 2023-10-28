aecfdc50249a080b24e52f709b036070f5cb1632
mrpp2001 <pratiknpatil2001@gmail.com>
2023-10-28 18:21:15 +0530

    Ui design done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  BsSearch,
-  BsThreeDotsVertical,
-  BsHeart,
-  BsHeartFill,
-  BsChat,
-} from "react-icons/bs";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 import search from "../assets/search.svg";
 import filters from "../assets/filters.svg";
@@ -13,6 +7,94 @@ import like from "../assets/like.svg";
 import comment from "../assets/comment.svg";
 import share from "../assets/share.svg";
 import unlike from "../assets/unlike.svg";
+
+import art1 from "../assets/art/Lara-art.png";
+import art2 from "../assets/art/thomas-art.png";
+
+import art3 from "../assets/art/thomas-ed-art.png";
+import art4 from "../assets/art/chris-art.png";
+import art5 from "../assets/art/Emilie-art.png";
+import art6 from "../assets/art/jessica-art.png";
+
+import user1 from "../assets/user/Lara-Leones.png";
+import user2 from "../assets/user/Thomas-J.png";
+
+// Art Post
+const Posts = [
+  {
+    id: 1,
+    userImg: user1,
+    userName: "Lara Leones",
+    userId: "@thewallart",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    art: art1,
+    likes: 9.8,
+    comments: 8.6,
+    share: 7.2,
+  },
+  {
+    id: 2,
+    userImg: user2,
+    userName: "Thomas J.",
+    userId: "@thecustomcreater",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    art: art2,
+    likes: 9.8,
+    comments: 8.6,
+    share: 7.2,
+  },
+];
+
+// Recommended Art
+const RecArtist = [
+  {
+    id: 1,
+    art: art6,
+    title: "Modern Wall Decor Framed Painting",
+    cost: 199.99,
+    star: "⭐⭐⭐⭐⭐",
+  },
+
+  {
+    id: 2,
+    art: art5,
+    title: "Modern Wall Decor Framed Painting",
+    cost: 199.99,
+    star: "⭐⭐⭐⭐⭐",
+  },
+
+  {
+    id: 3,
+    art: art3,
+    title: "Modern Wall Decor Framed Painting",
+    cost: 199.99,
+    star: "⭐⭐⭐⭐⭐",
+  },
+
+  {
+    id: 4,
+    art: art4,
+    title: "Modern Wall Decor Framed Painting",
+    cost: 199.99,
+    star: "⭐⭐⭐⭐⭐",
+  },
+
+  {
+    id: 5,
+    art: art5,
+    title: "Modern Wall Decor Framed Painting",
+    cost: 199.99,
+    star: "⭐⭐⭐⭐⭐",
+  },
+
+  {
+    id: 6,
+    art: art6,
+    title: "Modern Wall Decor Framed Painting",
+    cost: 199.99,
+    star: "⭐⭐⭐⭐⭐",
+  },
+];
 
 const Main = () => {
   return (
@@ -22,7 +104,11 @@ const Main = () => {
           className="flex relative justify-center w-[39rem] items-center"
           href=""
         >
-          <img className="absolute w-6 left-8 cursor-pointer" src={search} alt="" />
+          <img
+            className="absolute w-6 left-8 cursor-pointer"
+            src={search}
+            alt=""
+          />
           <input
             className="w-full py-6 px-16 h-20 bg-white rounded-lg "
             type="search"
@@ -34,248 +120,88 @@ const Main = () => {
           </button>
         </a>
 
-        {/* add h-screen to parent element */}
+        
         <div className="overflow-auto w-[39rem] no-scrollbar">
           <div className="flex flex-col gap-5 rounded-lg  ">
-            {/* POST 1 */}
-            <div className=" rounded-lg bg-white  h-[37.5rem]">
-              <div className="flex flex-col gap-8 p-8 border-b-2 border-gray-200">
-                <div className="flex justify-between">
-                  <a className=" flex gap-3" href="#">
+        
+
+            {Posts.map((post) => (
+              <div key={post.id} className=" rounded-lg bg-white  h-[37.5rem]">
+                <div className="flex flex-col gap-8 p-8 border-b-2 border-gray-200">
+                  <div className="flex justify-between">
+                    <a className=" flex gap-3" href="#">
+                      <img
+                        className="w-12 rounded-lg"
+                        src={post.userImg}
+                        alt=""
+                      />
+                      <div>
+                        <p className="text-xl font-bold">{post.userName}</p>
+                        <p className=" text-sm font-light">{post.userId}</p>
+                      </div>
+                    </a>
+                    <BsThreeDotsVertical className="text-2xl m-3" />
+                  </div>
+
+                  <p className="text-sm">
+                    {post.desc}
+                    <span className=" text-red-400">Read More</span>
+                  </p>
+
+                  <div className="relative">
                     <img
-                      className="w-12 rounded-lg"
-                      src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
+                      className="text-2xl text-white  absolute right-5 top-5"
+                      src={unlike}
                       alt=""
                     />
-                    <div>
-                      <p className="text-xl font-bold">Thomas Edward</p>
-                      <p className=" text-sm font-light">
-                        @thewildwithyou
-                      </p>
-                    </div>
-                  </a>
-                  <BsThreeDotsVertical className="text-2xl m-3" />
-                </div>
-
-                <p className="text-sm">
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.{" "}
-                  <span className=" text-red-400">Read More</span>
-                </p>
-
-                <div className="relative">
-                  <img className="text-2xl text-white  absolute right-5 top-5" src={unlike} alt="" />
-                  <img
-                    className="rounded-lg h-72 w-full"
-                    src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                    alt=""
-                  />
-                </div>
-              </div>
-
-              <div className="flex gap-5 p-8">
-                <p className="flex gap-3 text-xl">
-                  <img src={like} alt="" />
-                  <span>9.8k</span>
-                </p>
-                <p className="flex gap-3 text-xl">
-                <img src={comment} alt="" />
-                  <span>8.6k</span>
-                </p>
-                <p className="flex gap-3 text-xl">
-                <img src={share} alt="" />
-                  <span>7.2k</span>
-                </p>
-              </div>
-            </div>
-
-           
-            {/* POST 1 */}
-            <div className=" rounded-lg bg-white">
-              <div className="flex flex-col gap-8 p-8 border-b-2 border-gray-200">
-                <div className="flex justify-between">
-                  <a className=" flex gap-3" href="#">
                     <img
-                      className="w-12 rounded-lg"
-                      src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
+                      className="rounded-lg h-72 w-full"
+                      src={post.art}
                       alt=""
                     />
-                    <div>
-                      <p className="text-xl font-bold">Thomas Edward</p>
-                      <p className=" text-sm font-light">
-                        @thewildwithyou
-                      </p>
-                    </div>
-                  </a>
-                  <BsThreeDotsVertical className="text-2xl m-3" />
+                  </div>
                 </div>
 
-                <p className="text-sm">
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.{" "}
-                  <span className=" text-red-400">Read More</span>
-                </p>
-
-                <div className="relative">
-                  <img className="text-2xl text-white  absolute right-5 top-5" src={unlike} alt="" />
-                  <img
-                    className="rounded-lg h-72 w-full"
-                    src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                    alt=""
-                  />
+                <div className="flex gap-5 p-8">
+                  <p className="flex gap-3 text-xl">
+                    <img src={like} alt="" />
+                    <span>{post.likes} k</span>
+                  </p>
+                  <p className="flex gap-3 text-xl">
+                    <img src={comment} alt="" />
+                    <span>{post.comments} k</span>
+                  </p>
+                  <p className="flex gap-3 text-xl">
+                    <img src={share} alt="" />
+                    <span>{post.share} k</span>
+                  </p>
                 </div>
               </div>
-
-              <div className="flex gap-5 p-8">
-                <p className="flex gap-3 text-xl">
-                  <img src={like} alt="" />
-                  <span>9.8k</span>
-                </p>
-                <p className="flex gap-3 text-xl">
-                <img src={comment} alt="" />
-                  <span>8.6k</span>
-                </p>
-                <p className="flex gap-3 text-xl">
-                <img src={share} alt="" />
-                  <span>7.2k</span>
-                </p>
-              </div>
-            </div>
-
-           
+            ))}
+  
           </div>
-
-          {/* /////////////////////////  scrollbar //////////////////////////// */}
 
           <div className="flex gap-4 mt-5 overflow-x-auto no-scrollbar">
-
-
-            <div className="flex flex-col gap-3  h-full rounded-lg">
-              <img
-                className="rounded-lg h-44"
-                src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                alt=""
-              />
-              <p className="text-xs font-bold">
-                Modern Wall Decor Framed Painting
-              </p>
-              <div className="flex gap-16">
-                <p className="text-2xl font-bold">
-                  $<span>199.99</span>
+            {RecArtist.map((rec) => (
+              <div key={rec.id} className="flex flex-col gap-3  h-full rounded-lg">
+                <img
+                  className="rounded-lg h-44"
+                  src={rec.art}
+                  alt=""
+                />
+                <p className="text-xs font-bold">
+                 {rec.title}
                 </p>
-                <p className="my-2 text-sm">⭐⭐⭐⭐⭐</p>
+                <div className="flex gap-16">
+                  <p className="text-2xl font-bold">
+                    $<span>{rec.cost}</span>
+                  </p>
+                  <p className="my-2 text-sm">{rec.star}</p>
+                </div>
               </div>
-            </div>
-
-
-
-
-            <div className="flex flex-col gap-3  h-full rounded-lg">
-              <img
-                className="rounded-lg h-44"
-                src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                alt=""
-              />
-              <p className="text-xs font-bold">
-                Modern Wall Decor Framed Painting
-              </p>
-              <div className="flex gap-16">
-                <p className="text-2xl font-bold">
-                  $<span>199.99</span>
-                </p>
-                <p className="my-2 text-sm">⭐⭐⭐⭐⭐</p>
-              </div>
-            </div>
-
-
-
-
-            <div className="flex flex-col gap-3  h-full rounded-lg">
-              <img
-                className="rounded-lg h-44"
-                src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                alt=""
-              />
-              <p className="text-xs font-bold">
-                Modern Wall Decor Framed Painting
-              </p>
-              <div className="flex gap-16">
-                <p className="text-2xl font-bold">
-                  $<span>199.99</span>
-                </p>
-                <p className="my-2 text-sm">⭐⭐⭐⭐⭐</p>
-              </div>
-            </div>
-
-
-
-
-            <div className="flex flex-col gap-3  h-full rounded-lg">
-              <img
-                className="rounded-lg h-44"
-                src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                alt=""
-              />
-              <p className="text-xs font-bold">
-                Modern Wall Decor Framed Painting
-              </p>
-              <div className="flex gap-16">
-                <p className="text-2xl font-bold">
-                  $<span>199.99</span>
-                </p>
-                <p className="my-2 text-sm">⭐⭐⭐⭐⭐</p>
-              </div>
-            </div>
-
-
-
-
-            <div className="flex flex-col gap-3  h-full rounded-lg">
-              <img
-                className="rounded-lg h-44"
-                src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                alt=""
-              />
-              <p className="text-xs font-bold">
-                Modern Wall Decor Framed Painting
-              </p>
-              <div className="flex gap-16">
-                <p className="text-2xl font-bold">
-                  $<span>199.99</span>
-                </p>
-                <p className="my-2 text-sm">⭐⭐⭐⭐⭐</p>
-              </div>
-            </div>
-
-
-
-
-            <div className="flex flex-col gap-3  h-full rounded-lg">
-              <img
-                className="rounded-lg h-44"
-                src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                alt=""
-              />
-              <p className="text-xs font-bold">
-                Modern Wall Decor Framed Painting
-              </p>
-              <div className="flex gap-16">
-                <p className="text-2xl font-bold">
-                  $<span>199.99</span>
-                </p>
-                <p className="my-2 text-sm">⭐⭐⭐⭐⭐</p>
-              </div>
-            </div>
-
-
-
-
-
+            ))}
+          
           </div>
-
-
-
-
         </div>
       </div>
     </>
--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,12 +1,82 @@
 import React from "react";
 
+import user1 from "../assets/user/Thomas-Edward.png";
+import user2 from "../assets/user/Chris-Doe.png";
+import user3 from "../assets/user/Emilie-Jones.png";
+import user4 from "../assets/user/Jessica-Williams.png";
+
+import art1 from "../assets/art/thomas-ed-art.png";
+import art2 from "../assets/art/chris-art.png";
+import art3 from "../assets/art/Emilie-art.png";
+import art4 from "../assets/art/jessica-art.png";
+
+const Artist = [
+  {
+    id: 1,
+    userImg: user1,
+    userName: "Thomas Edward",
+    userId: "@thewildwithyou",
+    art: art1,
+  },
+  {
+    id: 2,
+    userImg: user2,
+    userName: "Chris Doe",
+    userId: "@thewildwithyou",
+    art: art2,
+  },
+  {
+    id: 3,
+    userImg: user3,
+    userName: "Emilie Jones",
+    userId: "@thewildwithyou",
+    art: art3,
+  },
+  {
+    id: 4,
+    userImg: user4,
+    userName: "Jessica Williams",
+    userId: "@thewildwithyou",
+    art: art4,
+  },
+  {
+    id: 5,
+    userImg: user1,
+    userName: "Lara Leones",
+    userId: "@thewallart",
+    art: art1,
+  },
+  {
+    id: 6,
+    userImg: user2,
+    userName: "Lara Leones",
+    userId: "@thewallart",
+    art: art2,
+  },
+  {
+    id: 7,
+    userImg: user3,
+    userName: "Lara Leones",
+    userId: "@thewallart",
+    art: art3,
+  },
+  {
+    id: 8,
+    userImg: user4,
+    userName: "Lara Leones",
+    userId: "@thewallart",
+    art: art4,
+  },
+];
+
 const Right = () => {
   return (
     <>
       <button className="rounded-lg w-64 h-20 mb-5 my-2 bg-[#88C2BB] text-white hover:bg-teal-400 text-base ">
         Become a Seller
       </button>
-      <div className="flex flex-col h-[81vh] text-sm    ">
+
+      <div className="flex flex-col h-[81vh] text-sm">
         <div className="flex gap-4 text-base mb-5">
           <a
             className="text-gray-500 hover:text-black focus:text-black hover:font-bold focus:font-bold "
@@ -23,137 +93,24 @@ const Right = () => {
         </div>
 
         <div className="flex flex-col gap-6 h-screen overflow-auto">
-          <div className="relative flex h-36 w-60">
-            <img
-              className="bg-cover rounded-xl"
-              src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-              alt=""
-            />
-            <a className="absolute flex gap-3 bottom-0 m-4 text-white" href="#">
-              <div className=" relative">
-                <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
-                <img
-                  className="w-12 rounded-lg border-2 border-white"
-                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                  alt=""
-                />
-              </div>
-              <p className=" text-base">
-                Thomas Edward <br />
-                <span className=" text-xs">@thewildwithyou</span>
-              </p>
-            </a>
-          </div>
-
-          <div className="relative flex h-36 w-60">
-            <img
-              className="bg-cover rounded-xl"
-              src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-              alt=""
-            />
-            <a className="absolute flex gap-3 bottom-0 m-4 text-white" href="#">
-              <div className=" relative">
-                <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
-                <img
-                  className="w-12 rounded-lg border-2 border-white"
-                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                  alt=""
-                />
-              </div>
-              <p className=" text-base">
-                Thomas Edward <br />
-                <span className=" text-xs">@thewildwithyou</span>
-              </p>
-            </a>
-          </div>
-
-          <div className="relative flex h-36 w-60">
-            <img
-              className="bg-cover rounded-xl"
-              src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-              alt=""
-            />
-            <a className="absolute flex gap-3 bottom-0 m-4 text-white" href="#">
-              <div className=" relative">
-                <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
-                <img
-                  className="w-12 rounded-lg border-2 border-white"
-                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                  alt=""
-                />
-              </div>
-              <p className=" text-base">
-                Thomas Edward <br />
-                <span className=" text-xs">@thewildwithyou</span>
-              </p>
-            </a>
-          </div>
-
-          <div className="relative flex h-36 w-60">
-            <img
-              className="bg-cover rounded-xl"
-              src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-              alt=""
-            />
-            <a className="absolute flex gap-3 bottom-0 m-4 text-white" href="#">
-              <div className=" relative">
-                <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
-                <img
-                  className="w-12 rounded-lg border-2 border-white"
-                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                  alt=""
-                />
-              </div>
-              <p className=" text-base">
-                Thomas Edward <br />
-                <span className=" text-xs">@thewildwithyou</span>
-              </p>
-            </a>
-          </div>
-
-          <div className="relative flex h-36 w-60">
-            <img
-              className="bg-cover rounded-xl"
-              src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-              alt=""
-            />
-            <a className="absolute flex gap-3 bottom-0 m-4 text-white" href="#">
-              <div className=" relative">
-                <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
-                <img
-                  className="w-12 rounded-lg border-2 border-white"
-                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                  alt=""
-                />
-              </div>
-              <p className=" text-base">
-                Thomas Edward <br />
-                <span className=" text-xs">@thewildwithyou</span>
-              </p>
-            </a>
-          </div>
-
-          <div className="relative flex h-36 w-60">
-            <img
-              className="bg-cover rounded-xl"
-              src="https://img.freepik.com/free-photo/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai_188544-7811.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-              alt=""
-            />
-            <a className="absolute flex gap-3 bottom-0 m-4 text-white" href="#">
-              <div className=" relative">
-                <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
-                <img
-                  className="w-12 rounded-lg border-2 border-white"
-                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=626&ext=jpg&ga=GA1.1.550779275.1698437704&semt=sph"
-                  alt=""
-                />
-              </div>
-              <p className=" text-base">
-                Thomas Edward <br />
-                <span className=" text-xs">@thewildwithyou</span>
-              </p>
-            </a>
-          </div>
+          {Artist.map((art) => (
+            <div key={art.id} className="relative flex h-36 w-60">
+              <img className="bg-cover rounded-xl brightness-50" src={art.art} alt="" />
+              <a
+                className="absolute flex gap-3 bottom-0 m-4 text-white"
+                href="#"
+              >
+                <div className=" relative">
+                  <span className="w-2.5 h-2.5 absolute rounded-full bg-green-300 border-2 border-white -top-0.5 -right-0.5"></span>
+                  <img className="w-12 rounded-lg" src={art.userImg} alt="" />
+                </div>
+                <p className=" text-base">
+                  {art.userName} <br />
+                  <span className=" text-xs">{art.userId}</span>
+                </p>
+              </a>
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex justify-between my-2 text-gray-500 text-xs">
