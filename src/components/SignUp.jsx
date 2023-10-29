1327847702481b2705e4e922fb24abaa49b07baa
mrpp2001 <pratiknpatil2001@gmail.com>
2023-10-29 23:03:48 +0530

    up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -51,13 +51,13 @@ const Signup = () => {
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label
-                    for="email"
+                    htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     Your Name
                   </label>
                   <input
-                    type="email"
+                    type="text"
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
@@ -73,7 +73,7 @@ const Signup = () => {
                 </div>
                 <div>
                   <label
-                    for="email"
+                    htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     Your email
@@ -95,7 +95,7 @@ const Signup = () => {
                 </div>
                 <div>
                   <label
-                    for="password"
+                    htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     Password
@@ -116,7 +116,7 @@ const Signup = () => {
                   />
                 </div>
                 <button
-                  type="submit"
+                  type="button"
                   className="w-full text-white bg-green-400 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   onClick={handleSubmission}
                   disabled={submitButtonDisabled}
@@ -126,7 +126,6 @@ const Signup = () => {
                 <p className="text-sm font-light text-gray-500">
                   Already have account?{" "}
                   <Link
-                    href="#"
                     className="font-medium text-red-500 hover:underline "
                     to="/"
                   >
